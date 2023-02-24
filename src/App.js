6fd0c9d30b3965665dd5f8507e05a57fdf27a70c
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-24 21:42:59 +0530

    Text editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+function TextEditor() {
+  const [text, setText] = useState('');
 
-function App() {
+  function handleInputChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleUppercaseClick() {
+    setText(text.toUpperCase());
+  }
+
+  function handleLowercaseClick() {
+    setText(text.toLowerCase());
+  }
+
+  function handleItalicClick() {
+    setText(text + ' ');
+    setText(text);
+  }
+
+  function handleUnderlineClick() {
+    setText(text + ' ');
+    setText(text);
+  }
+
+  function handleClearClick() {
+    setText('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <textarea className="input" value={text} onChange={handleInputChange} />
+
+      <div className="buttons">
+        <button className="uppercase" onClick={handleUppercaseClick}>Convert to Uppercase</button>
+        <button className="lowercase" onClick={handleLowercaseClick}>Convert to Lowercase</button>
+        <button className="italic" onClick={handleItalicClick}>Italic</button>
+        <button className="underline" onClick={handleUnderlineClick}>Underline</button>
+        <button className="clear" onClick={handleClearClick}>Clear Text</button>
+      </div>
+
+      <div className="output">
+        <p style={{ fontStyle: 'italic', textDecoration: 'underline' }}>{text}</p>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default TextEditor;
